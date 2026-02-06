edf3cff8580db8afae789e691f383a0355327c87
Bsuchot <bastien.suchot@gmail.com>
2026-02-06 11:59:52 +0100

    add 'page menu'

--- frontend/router/allroutes.js
+++ frontend/router/allroutes.js
@@ -2,7 +2,13 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/menu", "Menu", "/pages/menu.html"),
+    new Route("/contact", "Contact", "/pages/contact.html"),
+    new Route("/account", "Compte", "/pages/auth/account.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Vite & Gourmand";
