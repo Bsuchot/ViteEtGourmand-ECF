4ad906449d606f76b64172f98174f6b099b19b17
Bsuchot <bastien.suchot@gmail.com>
2026-03-17 12:43:09 +0100

    ajout utilisateurRepository

--- backend/src/Repository/UtilisateurRepository.php
+++ backend/src/Repository/UtilisateurRepository.php
@@ -4,11 +4,37 @@ namespace App\Repository;
 
 use App\Core\Repository;
 use App\Models\Utilisateur;
+use PDO;
 
 class UtilisateurRepository extends Repository
 {
-    public function hashPassword($password): void
+    public function findAll(): array
     {
-        $hash = password_hash($password, PASSWORD_DEFAULT);
+        $query = $this->pdo->prepare("SELECT * FROM `utilisateur`");
+        $query->execute();
+
+        $utilisateurs = $query->fetchAll($this->pdo::FETCH_ASSOC);
+
+        $utilisateursArray = [];
+        if ($utilisateurs) {
+            foreach ($utilisateurs as $utilisateurArray) {
+                $utilisateursArray[] = Utilisateur::createAndHydrate($utilisateurArray);
+            }
+        }
+
+        return $utilisateursArray;
+    }
+    public function findById(int $id): Utilisateur
+    {
+        $query = $this->pdo->prepare("SELECT * FROM `utilisateur` WHERE `id` = :id");
+        $query->bindValue(':id', $id, PDO::PARAM_INT);
+        $query->execute();
+        $utilisateurArray = $query->fetch($this->pdo::FETCH_ASSOC);
+
+        $utilisateur = Utilisateur::createAndHydrate($utilisateurArray);
+
+
+        return $utilisateur;
+
     }
 }
