d4f9f0f66461c609e33dbd62ef978a55d4c5f831
Bsuchot <bastien.suchot@gmail.com>
2026-05-21 08:58:58 +0200

    fix dotenv for heroku

--- backend/public/index.php
+++ backend/public/index.php
@@ -16,8 +16,10 @@ use App\Controllers\AvisController;
 use App\Controllers\EmployeController;
 
 require_once __DIR__ . '/../vendor/autoload.php';
-$dotenv = Dotenv::createImmutable(__DIR__.'/..');
-$dotenv->load();
+if (file_exists(__DIR__.'/../.env')) {
+    $dotenv = Dotenv::createImmutable(__DIR__.'/..');
+    $dotenv->load();
+}
 
 session_set_cookie_params([
     'lifetime' => 86400,
